42132bcd67bb322041a803e26a7cbdfe18222f6c
Frantz Degrigny <frantz.degrigny@keyconsulting.fr>
2015-08-18 11:51:05 +0200

    don't repeat yourself

--- java-model/src/main/java/fr/keyconsulting/formation/model/Operators.java
+++ java-model/src/main/java/fr/keyconsulting/formation/model/Operators.java
@@ -4,26 +4,26 @@ import java.util.HashMap;
 import java.util.Set;
 
 public abstract class Operators {
-	
-	private static HashMap<String,Operator> operators = new HashMap();
-	
-	static{
-		Operator plus = new PlusOperator();
-		operators.put(plus.getCode(),plus);
-		Operator minus = new MinusOperator();
-		operators.put(minus.getCode(),minus);
-		Operator multiply = new MultiplyOperator();
-		operators.put(multiply.getCode(),multiply);
-		Operator divide = new DivideOperator();
-		operators.put(divide.getCode(),divide);
+
+	private static HashMap<String, Operator> operators = new HashMap<>();
+
+	static {
+		registerOperator(new PlusOperator());
+		registerOperator(new MinusOperator());
+		registerOperator(new MultiplyOperator());
+		registerOperator(new DivideOperator());
 	}
-	
-	public static Set<String> all(){
+
+	public static Set<String> all() {
 		return operators.keySet();
-	}  
-	
-	public static Operator of(String opAsString){
+	}
+
+	public static Operator of(String opAsString) {
 		return operators.get(opAsString);
+	}
+
+	private static void registerOperator(Operator operator) {
+		operators.put(operator.getCode(), operator);
 	}
 
 }
